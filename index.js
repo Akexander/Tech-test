eb02107363607a414cc5158adc31c22610de16b9
Akexander <alexanderfazan@gmail.com>
2021-07-13 10:18:06 +0100

    second commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// alert("Hello Alex");
+
+// Random number generation between 0-500
+// let x = Math.floor(Math.random() * 500 + 1);
+// console.log(x);
+
+// number generation that can be mapped over
+// for (let i = 0; i < 501; i++) {
+//   console.log(i);
+// }
+
+
+
+
+if (let number = Math.floor((Math.random() * 500) + 1) {
+  let output = "";
+  if (number % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  prompt(output || number);
+}
+console.log(number)
